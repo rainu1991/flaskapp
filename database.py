eef6b15a0d961f7ee4abc30dc42a50a3eb835e71
rainu <rainu199@gmail.com>
2013-11-05 12:30:11 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,8 @@
+import psycopg2
+con = psycopg2.connect(database='rainu')    
+cur=con.cursor() 
+cur.execute("DROP TABLE IF EXISTS post")  
+cur.execute("CREATE TABLE post(id serial primary key,title TEXT,text TEXT,comment TEXT,time TEXT)")
+con.commit()
+con.close()
+
